9050a800d5eca048e788a43d798fd02fac63654b
YOCKOW <YOCKOW@users.noreply.github.com>
2024-01-30 11:09:52 +0900

    Initial commit.

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "PQ",
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(name: "CLibPQ", targets: ["CLibPQ"]),
+    .library(name: "SwiftPQ", targets: ["PQ"]),
+  ],
+  targets: [
+      // Targets are the basic building blocks of a package, defining a module or a test suite.
+      // Targets can depend on other targets in this package and products from dependencies.
+    .systemLibrary(
+      name: "CLibPQ",
+      pkgConfig: "libpq",
+      providers: [
+        .brew(["postgresql", "libpq"]),
+        .apt(["libpq-dev"]),
+      ]
+    ),
+    .target(name: "PQ", dependencies: ["CLibPQ"]),
+    .testTarget(name: "PQTests", dependencies: ["PQ"]),
+  ]
+)
+
---
+++ Tests/PQTests/PQTests.swift
@@ -0,0 +1,19 @@
+/* *************************************************************************************************
+ PQTests.swift
+   © 2024 YOCKOW.
+     Licensed under MIT License.
+     See "LICENSE.txt" for more information.
+ ************************************************************************************************ */
+
+import XCTest
+@testable import PQ
+
+final class SwiftPQTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
