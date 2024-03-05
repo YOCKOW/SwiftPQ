fd7fac8fb3cc2e91209c4325cdd1ad2a871c7293
YOCKOW <YOCKOW@users.noreply.github.com>
2024-03-05 15:47:03 +0900

    😅 Fix typos.

--- assets/grammars.zsh
+++ assets/grammars.zsh
@@ -3,15 +3,16 @@
 # Download gram.y file from PostgreSQL (mirror) repository
 # and let 'bison' parse the file to make it easy to read.
 
+set -u
 
 local -r assetsDir="$(cd "$(dirname $0)" && pwd -L)"
-local -r grammersDir="${assetsDir}/grammers"
+local -r grammarsDir="${assetsDir}/grammars"
 local -r gramyURL="https://raw.githubusercontent.com/postgres/postgres/master/src/backend/parser/gram.y"
-local -r gramyPath="${grammersDir}/gram.y"
+local -r gramyPath="${grammarsDir}/gram.y"
 
 set -ex
-mkdir -p "$grammersDir"
+mkdir -p "$grammarsDir"
 curl -sSL "$gramyURL" >"$gramyPath"
-cd "$grammersDir"
+cd "$grammarsDir"
 bison -v gram.y
 ls -ahl .
