643ff084623ac452564dea06e93a81046d5be319
YOCKOW <YOCKOW@users.noreply.github.com>
2024-03-05 10:21:50 +0900

    WIP: Start implementing `WithClause`.

---
+++ Sources/PQ/Commands/INSERT.swift
@@ -0,0 +1,13 @@
+/* *************************************************************************************************
+ INSERT.swift
+   © 2024 YOCKOW.
+     Licensed under MIT License.
+     See "LICENSE.txt" for more information.
+ ************************************************************************************************ */
+
+/// A representation of `INSERT` command.
+public struct Insert: SQLTokenSequence {
+  public var tokens: [SQLToken] {
+    return []
+  }
+}
