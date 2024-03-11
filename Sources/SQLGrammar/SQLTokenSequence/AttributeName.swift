f53682bb6870a08b0128ef6c71b8fb68767a0e3e
YOCKOW <YOCKOW@users.noreply.github.com>
2024-03-11 18:22:30 +0900

    Grammar: Add `AttributeName`.

---
+++ Sources/SQLGrammar/SQLTokenSequence/AttributeName.swift
@@ -0,0 +1,36 @@
+/* *************************************************************************************************
+ AttributeName.swift
+   © 2024 YOCKOW.
+     Licensed under MIT License.
+     See "LICENSE.txt" for more information.
+ ************************************************************************************************ */
+
+/// A type representing an attribute name which is described as `attr_name` in "gram.y".
+public enum AttributeName: SQLTokenSequence {
+  case columnLabel(ColumnLabel)
+
+  public init(_ label: ColumnLabel) {
+    self = .columnLabel(label)
+  }
+
+  public final class Iterator: IteratorProtocol {
+    public typealias Element = SQLToken
+
+    private let _iterator: AnySQLTokenSequenceIterator
+
+    fileprivate init(_ iterator: AnySQLTokenSequenceIterator) {
+      self._iterator = iterator
+    }
+
+    public func next() -> Element? {
+      return _iterator.next()
+    }
+  }
+
+  public func makeIterator() -> Iterator {
+    switch self {
+    case .columnLabel(let columnLabel):
+      return .init(AnySQLTokenSequenceIterator(columnLabel.token.asSequence))
+    }
+  }
+}
