0460394e33fbc06615a71efbdcc9170b4ecffa8b
YOCKOW <YOCKOW@users.noreply.github.com>
2024-03-14 12:58:32 +0900

    Grammar: Make `DropStatement` more generalized.

--- Sources/SQLGrammar/SQLTokenSequence/DropStatement.swift
+++ Sources/SQLGrammar/SQLTokenSequence/DropStatement.swift
@@ -6,30 +6,44 @@
  ************************************************************************************************ */
 
 /// A type representing "DROP" statement that is described as `DropStmt` in "gram.y".
-public protocol DropStatement: Statement {}
+public protocol DropStatement: Statement {
+  associatedtype ItemTypeName: SQLTokenSequence
+  associatedtype ItemNameList: SQLTokenSequence
+
+  var itemType: ItemTypeName { get }
+  var ifExists: Bool { get }
+  var itemNames: ItemNameList { get }
+  var behavior: DropBehavior? { get }
+}
+
+extension DropStatement where Self.Tokens == JoinedSQLTokenSequence {
+  public var tokens: JoinedSQLTokenSequence {
+    return JoinedSQLTokenSequence.compacting(
+      SingleToken(.drop),
+      itemType,
+      ifExists ? ifExistsSegment : nil,
+      itemNames,
+      behavior.map({ SingleToken($0) })
+    )
+  }
+}
 
 /// One of "DROP" statement whose object is an `ObjectTypeAnyName`.
 ///
 /// Represented statement is described as one of below  in "gram.y":
 /// - `DROP object_type_any_name IF_P EXISTS any_name_list opt_drop_behavior`
 /// - `DROP object_type_any_name any_name_list opt_drop_behavior`'
-public protocol DropObjectTypeAnyName: DropStatement {
+public protocol DropObjectTypeAnyName: DropStatement where ItemTypeName == ObjectTypeAnyName,
+                                                           ItemNameList == AnyNameList {
   var object: ObjectTypeAnyName { get }
   var ifExists: Bool { get }
   var names: AnyNameList { get }
   var behavior: DropBehavior? { get }
 }
 
-extension DropObjectTypeAnyName where Self.Tokens == JoinedSQLTokenSequence {
-  public var tokens: JoinedSQLTokenSequence {
-    return JoinedSQLTokenSequence.compacting(
-      SingleToken(.drop),
-      object,
-      ifExists ? ifExistsSegment : nil,
-      names,
-      behavior.map({ SingleToken($0) })
-    )
-  }
+extension DropObjectTypeAnyName {
+  public var itemType: ItemTypeName { return object }
+  public var itemNames: ItemNameList { return names }
 }
 
 /// Represents "DROP TABLE" statement.
